51b1a5fefb7f7246d896158c064857870c37b124
Kurt Daliuag <30821330+krutsednar@users.noreply.github.com>
2025-11-04 15:23:41 +0800

    Update LogoutResponse.php

--- app/Http/Responses/LogoutResponse.php
+++ app/Http/Responses/LogoutResponse.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Responses;
 
+use Filament\Facades\Filament;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Redirect;
 use Filament\Http\Responses\Auth\Contracts\LogoutResponse as Responsable;
@@ -10,6 +11,18 @@ class LogoutResponse implements Responsable
 {
     public function toResponse($request): RedirectResponse
     {
-        return Redirect::to('/home/logout');
+        // return Redirect::to('/home/login');
+        $panel = Filament::getCurrentPanel()?->getId();
+
+        return match ($panel) {
+            'home' => redirect('/home/login'),
+            'admin' => redirect('/admin/login'),
+            'MCIS' => redirect('/MCIS/login'),
+            'MOJO' => redirect('/MOJO/login'),
+            'MOCA' => redirect('/MOCA/login'),
+            'PFIS' => redirect('/PFIS/login'),
+            'executive' => redirect('/executive/login'),
+            default => redirect('/login'),
+        };
     }
 }
